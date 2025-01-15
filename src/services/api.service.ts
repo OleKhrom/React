6f32059906ba442416d797e7e69d075222b90cb5
OleKhrom <olekhrom@gmail.com>
2025-01-15 19:59:31 +0100

    Додано функціонал TodoComponent та оновлення компонентів

---
+++ src/services/api.service.ts
@@ -0,0 +1,8 @@
+import { ITodosResponse } from "../models/ITodosResponse.ts";
+
+const url = import.meta.env.VITE_API_URL;
+
+export const getAllTodos = async (): Promise<ITodosResponse> => {
+    const todosResponseObject = await fetch(`${url}/todos`).then((value) => value.json());
+    return todosResponseObject;
+};
