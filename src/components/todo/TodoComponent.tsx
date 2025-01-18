514eb7983b85730678e257266ef8d3c308d3efeb
OleKhrom <olekhrom@gmail.com>
2025-01-18 13:38:05 +0100

    home work 2.2gain was done

---
+++ src/components/todo/TodoComponent.tsx
@@ -0,0 +1,15 @@
+import {ITodo} from "../models/ITodo.ts";
+import './Todo.css';
+
+interface TodoComponentProps {
+    item: ITodo
+}
+
+export const TodoComponent = ({item}: TodoComponentProps) => {
+    return (
+        <div>
+            <h3 className={item.completed ? 'complete' : 'non-complete'}>{item.todo}</h3>
+        </div>
+    );
+};
+
