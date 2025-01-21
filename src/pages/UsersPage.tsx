b77ef73bbf5e4678d977fc613981930ec93cdd47
OleKhrom <olekhrom@gmail.com>
2025-01-21 14:34:29 +0100

    HW3.3 was edited

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,12 +1,8 @@
 import {UsersComponent} from "../components/users-component/UsersComponent.tsx";
-import {Outlet} from "react-router";
 
 export const UsersPage = () => {
     return (
         <div>
-            <>
-                <Outlet/>
-            </>
             <UsersComponent/>
         </div>
     );
